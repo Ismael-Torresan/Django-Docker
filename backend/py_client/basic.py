62070e0dd3ef9c75a0ff0e66e04b70a15ce57b87
Ismael <ismaeltorresan1@gmail.com>
2023-02-18 12:32:46 -0300

    generics api view

--- backend/py_client/basic.py
+++ backend/py_client/basic.py
@@ -3,6 +3,6 @@ import requests
 # endpoint = "https://httpbin.org/status/200"
 endpoint = "https://localhost:8000/api/products/products/"
 
-response = requests.get(endpoint)
+response = requests.post(endpoint, json={"title": "kalsdhfjk"})
 
 print(response.text)
