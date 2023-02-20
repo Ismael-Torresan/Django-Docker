ab27a9a9676cef425d416af5cc7c907af1970102
Ismael <ismaeltorresan1@gmail.com>
2023-02-20 12:23:11 -0300

    permissions

--- backend/py_client/basic.py
+++ backend/py_client/basic.py
@@ -1,8 +1,18 @@
 import requests
 
-endpoint = "http://localhost:8000/api/products/update/1/"
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/products"  # http://127.0.0.1:8000/
 
-data = {"title": "mandioca", "price": 128, "content": "macachera"}
+get_response = requests.post(
+    endpoint, json={"title": "Abc123", "content": "Hello world", "price": "abc134"}
+)  # HTTP Request
+# print(get_response.headers)
+# print(get_response.text) # print raw text response
+# print(get_response.status_code)
 
-response = requests.put(endpoint, json=data)
-print(response)
+# HTTP Request -> HTML
+# REST API HTTP Request -> JSON
+# JavaScript Object Nototion ~ Python Dict
+print(get_response.json())
+# print(get_response.status_code)
